39d62aa1cecaeb5c3a1fccda70889a96d8c0638f
Ying Lu <yinglu91@gmail.com>
2019-12-30 22:47:05 -0500

    update contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -55,6 +55,27 @@ const ContactState = props => {
     }
   };
 
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
+
   // Delete Contact
   const deleteContact = async id => {
     try {
@@ -79,11 +100,6 @@ const ContactState = props => {
   // Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // Update Contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // Filter Contacts
