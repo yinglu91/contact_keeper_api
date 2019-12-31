0c3a3206165782d5dc8366abda43847d4767a915
Ying Lu <yinglu91@gmail.com>
2019-12-31 10:38:25 -0500

    prepare deploy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-// app.get('/', (req, res) => res.send('Hello World123')); // http://localhost:5000/
+// app.get('/', (req, res) => res.send('Hello World')); // http://localhost:5000/
 // app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKeeper API' }));
 
 // Connect Database
@@ -16,6 +17,16 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
