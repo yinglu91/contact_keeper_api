b90cca882f7ddf7acea75d8b962ce4b786c1a74c
Ying Lu <yinglu91@gmail.com>
2019-12-31 10:38:02 -0500

    trivial change

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = ({ title, icon }) => {
     <>
       <li>Hello {user && user.name}</li>
       <li>
-        <a onClick={onLogout} href='#'>
+        <a onClick={onLogout} href='#!'>
           <i className='fas fa-sign-out-alt' />{' '}
           <span className='hide-sm'>Logout</span>
         </a>
--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -6,6 +6,7 @@ const Spinner = () => {
     <>
       <img
         src={spinner}
+        alt='Loading...'
         style={{ width: '200px', margin: 'auto', display: 'block' }}
       />
     </>
